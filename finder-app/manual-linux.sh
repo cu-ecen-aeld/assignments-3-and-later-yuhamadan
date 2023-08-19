a075daddb8b819520a2422492e8ed38b8a8f0e00
yuhamadan <yuhamadan@gmail.com>
2023-08-19 20:47:19 +0000

    test push

--- finder-app/manual-linux.sh
+++ finder-app/manual-linux.sh
@@ -37,76 +37,76 @@ fi
 ## for fixing issues
 # sudo apt install -y libssl-dev
 
-cd "$OUTDIR"
-if [ ! -d "${OUTDIR}/linux-stable" ]; then
-    #Clone only if the repository does not exist.
-	echo "CLONING GIT LINUX STABLE VERSION ${KERNEL_VERSION} IN ${OUTDIR}"
-	git clone ${KERNEL_REPO} --depth 1 --single-branch --branch ${KERNEL_VERSION}
-fi
-if [ ! -e ${OUTDIR}/linux-stable/arch/${ARCH}/boot/Image ]; then
-    cd linux-stable
-    echo "Checking out version ${KERNEL_VERSION}"
-    git checkout ${KERNEL_VERSION}
+# cd "$OUTDIR"
+# if [ ! -d "${OUTDIR}/linux-stable" ]; then
+#     #Clone only if the repository does not exist.
+# 	echo "CLONING GIT LINUX STABLE VERSION ${KERNEL_VERSION} IN ${OUTDIR}"
+# 	git clone ${KERNEL_REPO} --depth 1 --single-branch --branch ${KERNEL_VERSION}
+# fi
+# if [ ! -e ${OUTDIR}/linux-stable/arch/${ARCH}/boot/Image ]; then
+#     cd linux-stable
+#     echo "Checking out version ${KERNEL_VERSION}"
+#     git checkout ${KERNEL_VERSION}
 
-    # TODO: Add your kernel build steps here
-    echo "TODO: Add your kernel build steps here"
+#     # TODO: Add your kernel build steps here
+#     echo "TODO: Add your kernel build steps here"
 
-    ## “deep clean” the kernel build tree - removing the .config file with any existing configurations
-    make ARCH=arm64 CROSS_COMPILE=aarch64-none-linux-gnu- mrproper
+#     ## “deep clean” the kernel build tree - removing the .config file with any existing configurations
+#     make ARCH=arm64 CROSS_COMPILE=aarch64-none-linux-gnu- mrproper
     
-    ## Configure for our “virt” arm dev board we will simulate in QEMU
-    make ARCH=arm64 CROSS_COMPILE=aarch64-none-linux-gnu- defconfig
+#     ## Configure for our “virt” arm dev board we will simulate in QEMU
+#     make ARCH=arm64 CROSS_COMPILE=aarch64-none-linux-gnu- defconfig
 
-    ## Build a kernel image for booting with QEMU
-    make -j4 ARCH=arm64 CROSS_COMPILE=aarch64-none-linux-gnu- all
+#     ## Build a kernel image for booting with QEMU
+#     make -j4 ARCH=arm64 CROSS_COMPILE=aarch64-none-linux-gnu- all
 
-    ## Build any kernel modules - this step is being skipped in this assignment
-    # make ARCH=arm64 CROSS_COMPILE=aarch64-none-linux-gnu- modules
+#     ## Build any kernel modules - this step is being skipped in this assignment
+#     # make ARCH=arm64 CROSS_COMPILE=aarch64-none-linux-gnu- modules
 
-    ## Build the devicetree
-    make ARCH=arm64 CROSS_COMPILE=aarch64-none-linux-gnu- dtbs
+#     ## Build the devicetree
+#     make ARCH=arm64 CROSS_COMPILE=aarch64-none-linux-gnu- dtbs
 
-fi
+# fi
 
-echo "Adding the Image in outdir"
+# echo "Adding the Image in outdir"
 
-echo "Creating the staging directory for the root filesystem"
-cd "$OUTDIR"
-if [ -d "${OUTDIR}/rootfs" ]
-then
-	echo "Deleting rootfs directory at ${OUTDIR}/rootfs and starting over"
-    sudo rm  -rf ${OUTDIR}/rootfs
-fi
+# echo "Creating the staging directory for the root filesystem"
+# cd "$OUTDIR"
+# if [ -d "${OUTDIR}/rootfs" ]
+# then
+# 	echo "Deleting rootfs directory at ${OUTDIR}/rootfs and starting over"
+#     sudo rm  -rf ${OUTDIR}/rootfs
+# fi
 
-# TODO: Create necessary base directories
-echo "TODO: Create necessary base directories"
-ROOT_FS_DIR=${OUTDIR}/rootfs
-mkdir -p $ROOT_FS_DIR
-cd "$ROOT_FS_DIR"
-mkdir -p bin dev etc home lib lib64 proc sbin sys tmp usr var
-mkdir -p usr/bin usr/lib usr/sbin
-mkdir -p var/log
-
-
-cd "$OUTDIR"
-if [ ! -d "${OUTDIR}/busybox" ]
-then
-git clone git://busybox.net/busybox.git
-    cd busybox
-    git checkout ${BUSYBOX_VERSION}
-    # TODO:  Configure busybox
-    echo "TODO:  Configure busybox ??"
-else
-    cd busybox
-fi
+# # TODO: Create necessary base directories
+# echo "TODO: Create necessary base directories"
+# ROOT_FS_DIR=${OUTDIR}/rootfs
+# mkdir -p $ROOT_FS_DIR
+# cd "$ROOT_FS_DIR"
+# mkdir -p bin dev etc home lib lib64 proc sbin sys tmp usr var
+# mkdir -p usr/bin usr/lib usr/sbin
+# mkdir -p var/log
 
 
-# TODO: Make and install busybox
-echo "TODO: Make and install busybox"
-make distclean
-make defconfig
-make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE}
-make CONFIG_PREFIX=$ROOT_FS_DIR ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} install
+# cd "$OUTDIR"
+# if [ ! -d "${OUTDIR}/busybox" ]
+# then
+# git clone git://busybox.net/busybox.git
+#     cd busybox
+#     git checkout ${BUSYBOX_VERSION}
+#     # TODO:  Configure busybox
+#     echo "TODO:  Configure busybox ??"
+# else
+#     cd busybox
+# fi
+
+
+# # TODO: Make and install busybox
+# echo "TODO: Make and install busybox"
+# make distclean
+# make defconfig
+# make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE}
+# make CONFIG_PREFIX=$ROOT_FS_DIR ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} install
 
 
 echo "Library dependencies"
@@ -141,6 +141,7 @@ sudo mknod -m 600 dev/console c 5 1
 # TODO: Copy the finder related scripts and executables to the /home directory
 echo "TODO: Copy the finder related scripts and executables to the /home directory"
 # on the target rootfs
+mkdir -p $ROOT_FS_DIR/home
 cp -r $HOME/workspace/coursera/Linux-System-Programming-and-Introduction-to-Buildroot/assignment-1-yuhamadan/finder-app/* ${ROOT_FS_DIR}/home/
 
 # TODO: Clean and build the writer utility
