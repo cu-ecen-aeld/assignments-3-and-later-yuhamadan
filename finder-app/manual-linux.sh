e182ab1965d183894694f78611b32afefc0532a9
yuhamadan <yuhamadan@gmail.com>
2023-08-20 07:07:03 +0000

    another full run try

--- finder-app/manual-linux.sh
+++ finder-app/manual-linux.sh
@@ -35,103 +35,83 @@ if [ ! -d ${OUTDIR} ] ; then
 fi
 
 
-mkdir -p /tmp/lib64/
-mkdir -p /tmp/home/
-mkdir -p /tmp/lib/
-
-ARM_CC_LOCATION=/usr/local/arm-cross-compiler/
-finder_app_location=$(find /__w/assignments-3-and-later-yuhamadan/ -name finder-app/ -print -quit)
-somelib_location=$(find $ARM_CC_LOCATION -name ld-linux-aarch64.so.1 -print -quit)
-
-cp $somelib_location /tmp/lib/
-
-# LIB_86_64_DIR=/lib/x86_64-linux-gnu
-LIB_86_64_DIR=$(find $ARM_CC_LOCATION -name libm.so.6 -print -quit)
-LIB_86_64_DIR=$(dirname "$LIB_86_64_DIR")
-cp $LIB_86_64_DIR/libm.so.6 $LIB_86_64_DIR/libresolv.so.2 $LIB_86_64_DIR/libc.so.6 /tmp/lib64/
-
-cp -r $finder_app_location/* /tmp/home/
-
-echo "it worked!"
-exit 0
-
-# cp -r /home/mansul/workspace/coursera/Linux-System-Programming-and-Introduction-to-Buildroot/assignment-1-yuhamadan/finder-app/* /tmp/home/
-# cp /usr/aarch64-linux-gnu/lib/ld-linux-aarch64.so.1 /tmp/lib/
+cp -r /home/mansul/workspace/coursera/Linux-System-Programming-and-Introduction-to-Buildroot/assignment-1-yuhamadan/finder-app/* /tmp/home/
+cp /usr/aarch64-linux-gnu/lib/ld-linux-aarch64.so.1 /tmp/lib/
 
 
-## for fixing issues
-# sudo apt install -y libssl-dev
+# for fixing issues
+sudo apt install -y libssl-dev
 
-# cd "$OUTDIR"
-# if [ ! -d "${OUTDIR}/linux-stable" ]; then
-#     #Clone only if the repository does not exist.
-# 	echo "CLONING GIT LINUX STABLE VERSION ${KERNEL_VERSION} IN ${OUTDIR}"
-# 	git clone ${KERNEL_REPO} --depth 1 --single-branch --branch ${KERNEL_VERSION}
-# fi
-# if [ ! -e ${OUTDIR}/linux-stable/arch/${ARCH}/boot/Image ]; then
-#     cd linux-stable
-#     echo "Checking out version ${KERNEL_VERSION}"
-#     git checkout ${KERNEL_VERSION}
+cd "$OUTDIR"
+if [ ! -d "${OUTDIR}/linux-stable" ]; then
+    #Clone only if the repository does not exist.
+	echo "CLONING GIT LINUX STABLE VERSION ${KERNEL_VERSION} IN ${OUTDIR}"
+	git clone ${KERNEL_REPO} --depth 1 --single-branch --branch ${KERNEL_VERSION}
+fi
+if [ ! -e ${OUTDIR}/linux-stable/arch/${ARCH}/boot/Image ]; then
+    cd linux-stable
+    echo "Checking out version ${KERNEL_VERSION}"
+    git checkout ${KERNEL_VERSION}
 
-#     # TODO: Add your kernel build steps here
-#     echo "TODO: Add your kernel build steps here"
+    # TODO: Add your kernel build steps here
+    echo "TODO: Add your kernel build steps here"
 
-#     ## “deep clean” the kernel build tree - removing the .config file with any existing configurations
-#     make ARCH=arm64 CROSS_COMPILE=aarch64-none-linux-gnu- mrproper
+    ## “deep clean” the kernel build tree - removing the .config file with any existing configurations
+    make ARCH=arm64 CROSS_COMPILE=aarch64-none-linux-gnu- mrproper
     
-#     ## Configure for our “virt” arm dev board we will simulate in QEMU
-#     make ARCH=arm64 CROSS_COMPILE=aarch64-none-linux-gnu- defconfig
+    ## Configure for our “virt” arm dev board we will simulate in QEMU
+    make ARCH=arm64 CROSS_COMPILE=aarch64-none-linux-gnu- defconfig
 
-#     ## Build a kernel image for booting with QEMU
-#     make -j4 ARCH=arm64 CROSS_COMPILE=aarch64-none-linux-gnu- all
+    ## Build a kernel image for booting with QEMU
+    make -j4 ARCH=arm64 CROSS_COMPILE=aarch64-none-linux-gnu- all
 
-#     ## Build any kernel modules - this step is being skipped in this assignment
-#     # make ARCH=arm64 CROSS_COMPILE=aarch64-none-linux-gnu- modules
+    ## Build any kernel modules - this step is being skipped in this assignment
+    # make ARCH=arm64 CROSS_COMPILE=aarch64-none-linux-gnu- modules
 
-#     ## Build the devicetree
-#     make ARCH=arm64 CROSS_COMPILE=aarch64-none-linux-gnu- dtbs
+    ## Build the devicetree
+    make ARCH=arm64 CROSS_COMPILE=aarch64-none-linux-gnu- dtbs
 
-# fi
+fi
 
-# echo "Adding the Image in outdir"
+echo "Adding the Image in outdir"
 
-# echo "Creating the staging directory for the root filesystem"
-# cd "$OUTDIR"
-# if [ -d "${OUTDIR}/rootfs" ]
-# then
-# 	echo "Deleting rootfs directory at ${OUTDIR}/rootfs and starting over"
-#     sudo rm  -rf ${OUTDIR}/rootfs
-# fi
+echo "Creating the staging directory for the root filesystem"
+cd "$OUTDIR"
+if [ -d "${OUTDIR}/rootfs" ]
+then
+	echo "Deleting rootfs directory at ${OUTDIR}/rootfs and starting over"
+    sudo rm  -rf ${OUTDIR}/rootfs
+fi
 
-# # TODO: Create necessary base directories
-# echo "TODO: Create necessary base directories"
-# ROOT_FS_DIR=${OUTDIR}/rootfs
-# mkdir -p $ROOT_FS_DIR
-# cd "$ROOT_FS_DIR"
-# mkdir -p bin dev etc home lib lib64 proc sbin sys tmp usr var
-# mkdir -p usr/bin usr/lib usr/sbin
-# mkdir -p var/log
-
-
-# cd "$OUTDIR"
-# if [ ! -d "${OUTDIR}/busybox" ]
-# then
-# git clone git://busybox.net/busybox.git
-#     cd busybox
-#     git checkout ${BUSYBOX_VERSION}
-#     # TODO:  Configure busybox
-#     echo "TODO:  Configure busybox ??"
-# else
-#     cd busybox
-# fi
+# TODO: Create necessary base directories
+echo "TODO: Create necessary base directories"
+ROOT_FS_DIR=${OUTDIR}/rootfs
+mkdir -p $ROOT_FS_DIR
+cd "$ROOT_FS_DIR"
+mkdir -p bin dev etc home lib lib64 proc sbin sys tmp usr var
+mkdir -p usr/bin usr/lib usr/sbin
+mkdir -p var/log
 
 
-# # TODO: Make and install busybox
-# echo "TODO: Make and install busybox"
-# make distclean
-# make defconfig
-# make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE}
-# make CONFIG_PREFIX=$ROOT_FS_DIR ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} install
+cd "$OUTDIR"
+if [ ! -d "${OUTDIR}/busybox" ]
+then
+git clone git://busybox.net/busybox.git
+    cd busybox
+    git checkout ${BUSYBOX_VERSION}
+    # TODO:  Configure busybox
+    echo "TODO:  Configure busybox ??"
+else
+    cd busybox
+fi
+
+
+# TODO: Make and install busybox
+echo "TODO: Make and install busybox"
+make distclean
+make defconfig
+make ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE}
+make CONFIG_PREFIX=$ROOT_FS_DIR ARCH=${ARCH} CROSS_COMPILE=${CROSS_COMPILE} install
 
 
 echo "Library dependencies"
@@ -153,7 +133,16 @@ cd "$ROOT_FS_DIR"
 
 # TODO: Add library dependencies to rootfs
 echo "TODO: Add library dependencies to rootfs"
-LIB_86_64_DIR=/lib/x86_64-linux-gnu
+ARM_CC_LOCATION=/usr/local/arm-cross-compiler/
+somelib_location=$(find $ARM_CC_LOCATION -name ld-linux-aarch64.so.1 -print -quit)
+
+cp $somelib_location ${ROOT_FS_DIR}/lib/
+
+# LIB_86_64_DIR=/lib/x86_64-linux-gnu
+LIB_86_64_DIR=$(find $ARM_CC_LOCATION -name libm.so.6 -print -quit)
+LIB_86_64_DIR=$(dirname "$LIB_86_64_DIR")
+cp $LIB_86_64_DIR/libm.so.6 $LIB_86_64_DIR/libresolv.so.2 $LIB_86_64_DIR/libc.so.6 ${ROOT_FS_DIR}/lib64/
+
 
 # TODO: Make device nodes
 echo "TODO: Make device nodes"
@@ -164,7 +153,8 @@ sudo mknod -m 600 dev/console c 5 1
 echo "TODO: Copy the finder related scripts and executables to the /home directory"
 # on the target rootfs
 mkdir -p $ROOT_FS_DIR/home
-cp -r /home/mansul/workspace/coursera/Linux-System-Programming-and-Introduction-to-Buildroot/assignment-1-yuhamadan/finder-app/* ${ROOT_FS_DIR}/home/
+finder_app_location=$(find /__w/assignments-3-and-later-yuhamadan/ -name finder-app/ -print -quit)
+cp -r $finder_app_location/* ${ROOT_FS_DIR}/home/
 
 # TODO: Clean and build the writer utility
 echo "TODO: Clean and build the writer utility"
